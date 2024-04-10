09bd91120321ef3c0e2338d139de04aabc6db639
LucasPerezDev@gmail.com <LucasPerezDev@gmail.com>
2024-04-10 16:19:16 -0400

    Added components folder

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceHolderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer PlaceHolderImageSource={PlaceHolderImage} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
