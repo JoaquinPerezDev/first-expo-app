adab215906f923a59130970c9f40ea7030e1f9b0
LucasPerezDev@gmail.com <LucasPerezDev@gmail.com>
2024-04-10 16:56:54 -0400

    Added ImagePicker to App.js

--- App.js
+++ App.js
@@ -2,10 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceHolderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+
+  const PickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if(!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+  
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
